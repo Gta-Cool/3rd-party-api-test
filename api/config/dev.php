b35c86626d0aeea9f68a4ce7f3ba9a46e48aeee6
Jonathan Plugaru <jplugaru@hotmail.fr>
2016-09-12 07:22:50 +0100

    Removed unused things

--- api/config/dev.php
+++ api/config/dev.php
@@ -1,6 +1,8 @@
 <?php
 
+use Silex\Provider\HttpFragmentServiceProvider;
 use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\WebProfilerServiceProvider;
 
 // include the prod configuration
@@ -8,6 +10,13 @@ require __DIR__.'/prod.php';
 
 // enable the debug mode
 $app['debug'] = true;
+
+$app['twig.path'] = array(__DIR__.'/../templates');
+$app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+
+$app->register(new TwigServiceProvider());
+$app->register(new HttpFragmentServiceProvider());
+
 
 $app->register(new MonologServiceProvider(), array(
     'monolog.logfile' => __DIR__.'/../var/logs/silex_dev.log',
